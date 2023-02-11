a5e69a72eb724eaf238514111eb12fd71544419a
jonas walden <smodcartoon@gmail.com>
2023-02-11 16:55:07 -0500

    still fixing stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
-/** @type {import('tailwindcss').Config} \*/
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ['var(--font-montserrat)'],
-    },
+    extend: {},
   },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [require('@tailwindcss/aspect-ratio')],
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
